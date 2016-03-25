22290c692717fa547ed9a64a5d82b62290ad84ed
Bruno Fernandes <bdpf5@hotmail.com>
2016-03-25 17:36:43 +0000

    pages images

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>A sua mensagem foi enviada com sucesso. </strong>");
+                        .append("<strong>A tua mensagem foi enviada com sucesso. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +45,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", parece que o servidor de email não está a responder. Tente novamente mais tarde!");
+                    $('#success > .alert-danger').append("<strong>Desculpa " + firstName + ", parece que o servidor de email não está a responder. Tenta novamente mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
